8741d08397c2f9804a2ac74a71df804707241da1
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-29 19:09:06 -0400

    Update long_mpc.py

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -12,8 +12,8 @@ from selfdrive.controls.lib.drive_helpers import MPC_COST_LONG
 
 LOG_MPC = os.environ.get('LOG_MPC', False)
 
-BpTr = [0.,  .2,  5., 20.9, 21., 30., 36.]
-TrY = [.85, 1.8, 2.,  1.8, 1.8, 1.6, .85]
+BpTr = [0., 2.7, 2.8, 5., 21., 30., 36.]
+TrY = [.85, .85, 1.8, 2., 1.8, 1.6, .85]
 
 class LongitudinalMpc():
   def __init__(self, mpc_id):
